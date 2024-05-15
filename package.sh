a453af202afeb7161a1c8a728ea60fa985380dd1
neel2751 <neel2751@users.noreply.github.com>
2024-05-15 15:35:26 +0000

    deploy: cf1e747c3f45934d9010af7edb1a9bfae0886ea0

---
+++ package.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+declare -r app_name="package-demo"
+declare -r app_version="1.0.0"
+# Stops execution of the script if error
+set -e
+# Prints each command before execution
+
+npm run build
+
+zip -r app.zip .next package.json
